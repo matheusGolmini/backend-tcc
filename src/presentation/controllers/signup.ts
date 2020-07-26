9a776bfa46725a55688adbca645d5153d7342b55
Matheus Fernando <matheus.golmini@wisereducacao.com>
2020-07-26 11:48:58 -0300

    feat: add SignUpController

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,38 @@
+import { Controller } from '../protocols/controller'
+import { MissingParamError } from '../errors/missing-param-error'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { serverError, badRequest } from '../helpers/http-helpers'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
+
+export class SignUpController implements Controller {
+    private readonly emailValidator: EmailValidator
+
+    constructor (emailValidator: EmailValidator) {
+      this.emailValidator = emailValidator
+    }
+
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
+            }
+            const { email } = httpRequest.body
+            const isValid = this.emailValidator.isValid(email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+
+            return {
+                body: '',
+                statusCode: 200
+            }
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
